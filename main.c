5287fe8c8e28410fea31c9da70047172bc6852c4
davipucodigo <davi.santvieira@gmail.com>
2025-03-27 13:21:20 +0000

    principal

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+#include <time.h>
+
+void Menu () {
+    printf("\n ███╗   ███╗███████╗ ██████╗  █████╗     ████████╗██████╗  ██████╗  ");
+    printf("\n ████╗ ████║██╔════╝██╔════╝ ██╔══██╗    ╚══██╔══╝██╔══██╗██╔═══██╗ ");
+    printf("\n ██╔████╔██║█████╗  ██║  ███╗███████║       ██║   ██████╔╝██║   ██║ ");
+    printf("\n ██║╚██╔╝██║██╔══╝  ██║   ██║██╔══██║       ██║   ██╔══██╗██║   ██║ ");
+    printf("\n ██║ ╚═╝ ██║███████╗╚██████╔╝██║  ██║       ██║   ██║  ██║╚██████╔╝ ");
+    printf("\n ╚═╝     ╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝       ╚═╝   ╚═╝  ╚═╝ ╚═════╝  ");
+    printf("\n DAVI SANT'ANNA - TRABALHO - TURMA 4324 \n");
+}
 
 
 int main () {
+    //CONFIGURAÇÕES:
+    setlocale(LC_ALL,"Portuguese");
+    srand(time(NULL));
+
+    //MENU:
+    Menu();
     return 0;
 }
