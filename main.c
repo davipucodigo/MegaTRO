39907afae6e8548b1e4d47ca3e8194de041ab775
davipucodigo <davi.santvieira@gmail.com>
2025-03-27 13:55:21 +0000

    Definindo defines

--- main.c
+++ main.c
@@ -3,6 +3,34 @@
 #include <locale.h>
 #include <time.h>
 
+//SORTEIO 6 numeros compreendidos entre 1 a 60
+#define NUMERO_MAX_SORTEADO 60
+
+//NUMEROS DO USUARIO
+#define MAX_NUMEROS_APOSTA 10
+#define MIN_NUMEROS_APOSTA 6
+
+//CUSTOS dos NUMEROS APOSTADOS
+#define SEIS_NUM_CUSTO 2
+#define SETE_NUM_CUSTO 20
+#define OITO_NUM_CUSTO 60
+#define NOVE_NUM_CUSTO 120
+#define DEZ_NUM_CUSTO 450
+
+//PONTUAÇÔES DEBITAVEIS
+#define QUADRA 4
+#define QUINA 5
+#define MEGATRO 6
+
+//DEBITOS
+#define PAGA_QUADRA 1650
+#define PAGA_QUINA 15000
+#define PAGA_MEGATRO 5000000
+
+//TECLA DE SAIDA
+#define ESC_TECLA 27
+
+
 void Menu () {
     printf("\n ███╗   ███╗███████╗ ██████╗  █████╗     ████████╗██████╗  ██████╗  ");
     printf("\n ████╗ ████║██╔════╝██╔════╝ ██╔══██╗    ╚══██╔══╝██╔══██╗██╔═══██╗ ");
@@ -12,7 +40,6 @@ void Menu () {
     printf("\n ╚═╝     ╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝       ╚═╝   ╚═╝  ╚═╝ ╚═════╝  ");
     printf("\n DAVI SANT'ANNA - TRABALHO - TURMA 4324 \n");
 }
-
 
 int main () {
     //CONFIGURAÇÕES:
